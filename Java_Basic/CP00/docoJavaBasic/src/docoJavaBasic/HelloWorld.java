ae46dda74797b901b146e7468d2af727d85a23b3
onesword0618 <ao.akua.leo@gmail.com>
2018-04-09 23:35:16 +0900

    スッキリわかるJava入門-Javaをはじめよう
    CP00 Javaをはじめよう
    P.18-23
    文章の表示と簡単な加算計算

---
+++ Java_Basic/CP00/docoJavaBasic/src/docoJavaBasic/HelloWorld.java
@@ -0,0 +1,10 @@
+package docoJavaBasic;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World"); //$NON-NLS-1$
+		System.out.println("31 + 31の計算をします"); //$NON-NLS-1$
+		System.out.println(31 + 31);
+	}
+}
