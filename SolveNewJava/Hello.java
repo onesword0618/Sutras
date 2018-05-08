c040ec6551c881abc14ba23e9dfc5852938795e8
onesword0618 <ao.akua.leo@gmail.com>
2018-05-08 22:02:47 +0900

    新・解きながら学ぶJava
    問題1-1
    	コンソール画面に「Java's Program」,「Out put Console」
    		クラス宣言
    			クラス名の先頭は大文字で表現すること
    			ソースファイル名と完全一致させること
    		文字リテラル
    			二重引用符で囲んだ文字の並びは文字列リテラルという
    			リテラルは「文字通りの」という意味
    
    問題1-2
    	セミコロンを削って、プログラムをコンパイルして検証してみる
    		コンパイルエラーになる
    		コンソール画面を含めて、外部との入出力に利用するのが、文字が流れる川に見立ててストリームという
    		System.out はコンソール画面と結びつけている標準出力ストリーム
    		println はコンソール画面に出力して「改行」する部品(メソッド)
    		※コメントは入れ子にすることはできない

---
+++ SolveNewJava/Hello.java
@@ -0,0 +1,8 @@
+//Out put Console
+class Hello {
+
+  public static void main(String[] args) {
+    System.out.println("Java's Program");
+    System.out.println("Out put Console");
+  }
+}
---
+++ SolveNewJava/HelloError.java
@@ -0,0 +1,8 @@
+// Out put Console error
+class HelloError {
+
+  public static void main(String[] args){
+    System.out.println("Java's Program")
+    System.out.println("Out put Console")
+  }
+}
