b0a3ed49199520f3b0417b15a083ea4789590714
onesword0618 <ao.akua.leo@gmail.com>
2018-05-09 23:45:15 +0900

    新・解きながら学ぶJava
    問題1-3
    		「初めてのプログラム。」と「画面に出力しています。」を、改行することなく連続して表示するプログラムを作成せよ。
    
    		System.out.println で出力している。
    		System.out.printlnの「ln」 はlineの略。
    		これを取り除くと改行されずに出力される。
    		System.out.print
    		文字列リテラル同士を「＋」で連結すると連結された状態の文字列が生成される。

---
+++ SolveNewJava/Hello1A.java
@@ -0,0 +1,7 @@
+// 画面への連続表示
+class Hello1A {
+
+	public static void main (String[] args) {
+		System.out.println("初めてのJavaプログラム。画面に出力しています。");
+	}
+}
---
+++ SolveNewJava/Hello1C.java
@@ -0,0 +1,7 @@
+// 画面への連続表示
+class Hello1C {
+
+	public static void main (String[] args) {
+		System.out.println("初めてのJavaプログラム。" + "画面に出力しています。");
+	}
+}
