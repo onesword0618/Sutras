c2b1ee0c18ff8f18a1684bc2bf974112bfaa3961
onesword0618 <ao.akua.leo@gmail.com>
2018-05-10 23:23:53 +0900

    新・解きながら学ぶJava
    問題1-4
    	各行に一文字ずつ文字を表示するプログラムを作成せよ。
    
    	改行
    		printlnで改行して出力されることを確認すること
    	改行文字
    		\nで表すのは改行文字という単一の文字
    		目に見える文字としての表記が不可能あるいは、困難な文字は、逆斜線\で始まる拡張表記で表す。
    
    問題1-5
    	各行に一文字ずつ文字を表示するプログラムを作成せよ。間を開けて作成すること。
    
    	空白
    		println();で改行文字を出力している。
    		printでは改行文字を出力することが出来ない。
    	改行文字による空白
    		\n\nで空白を作ることができる。
    	※コメントアウト
    	一時的に無効にして動作確認を行うことができる。

---
+++ SolveNewJava/PrintName1A.java
@@ -0,0 +1,10 @@
+// ©•ª‚Ì–¼‘O‚ğ1s‚É1•¶š‚¸‚Â•\¦‚·‚é(‚»‚Ì1)
+class PrintName1A {
+
+	public static void main(String[] args) {
+		System.out.println("“Œ");
+		System.out.println("¼");
+		System.out.println("“ì");
+		System.out.println("–k");
+	}
+}
