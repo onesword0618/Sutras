1e8051ee75e4a971e5e99aacef5511e2b4e590a5
onesword0618 <ao.akua.leo@gmail.com>
2018-05-29 23:12:59 +0900

    新・解きながら学ぶJava
    問題2-14
    	名前と姓と名とを個別にキーボードから読み込んで、挨拶を行うプログラムを作成せよ。
    		文字列を扱うプログラム。読み込んだ文字列を格納する変数lastName,firstNameはいずれも文字列を表すString型である。
    		＋演算子で連結することもできる

---
+++ SolveNewJava/Greeting.java
@@ -0,0 +1,14 @@
+// ê©Ç∆ñºÇì«Ç›çûÇÒÇ≈à•éA
+import java.util.Scanner;
+
+class Greeting {
+
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+	
+		System.out.print("ê©ÅF"); String lastName = stdIn.next();
+		System.out.print("ñºÅF"); String firstName = stdIn.next();
+		
+		System.out.print("Ç±ÇÒÇ…ÇøÇÕ" + lastName + firstName + "Ç≥ÇÒ");
+	}
+}
