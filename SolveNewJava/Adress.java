cae3342ee2dd87f2701028cdb4d6884fe581b3e3
onesword0618 <ao.akua.leo@gmail.com>
2018-05-31 02:15:47 +0900

    新・解きながら学ぶJava
    問題2-15
    	住所を読み込んで反復表示
    		文字列の読込では、nextメソッドを使います。ただし、next()によるキーボードからの読込では、空白文字やタブ文字が文字列の区切りとみなされます。
    		スペースも含めた1行分の入力を文字列として読み込むには、nextLineメソッドを使わなればなりません。住所に空白文字が使われる可能性があるので、nextLine()を使っている。

---
+++ SolveNewJava/Adress.java
@@ -0,0 +1,14 @@
+// ZŠ‚ğ“Ç‚İ‚ñ‚Å”½•œ•\¦B
+import java.util.Scanner;
+
+class Adress {
+
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+	
+		System.out.print("ZŠF"); 
+		String adress = stdIn.nextLine();
+		
+		System.out.print("‚¨Z‚Ü‚¢‚Í" + adress + "‚Å‚·‚ËB");
+	}
+}
