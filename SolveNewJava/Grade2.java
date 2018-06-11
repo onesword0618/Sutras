a86fe97d2cebda0f4d8e06d0d79e612c8ae643a3
onesword0618 <ao.akua.leo@gmail.com>
2018-06-12 01:12:46 +0900

    新・解きながら学ぶJava
    問題3-11
    	キーボードから読み込んだ点数に応じて、優/良/可/不可を判定して表示するプログラムを作成せよ。判定は以下のように行うこと。その2
    
    	論理和演算子　||
    	ｘまたはy
    
    	||は縦線記号
    	評価を少なくできる分こちらを使うことが多いかも。

---
+++ SolveNewJava/Grade2.java
@@ -0,0 +1,21 @@
+// 点数をもとに成績を判定
+import java.util.Scanner;
+
+class Grade2 {
+
+  public static void main(String[] args) {
+    Scanner stdIn = new Scanner(System.in);
+
+    System.out.print("点数：");
+    int point = stdIn.nextInt();
+
+    if (point < 0 || point > 100)
+      System.out.println("不正な点数です。");
+    else if (point <= 59)
+      System.out.println("不可");
+    else if (point <= 69)
+      System.out.println("良");
+    else
+      System.out.println("優");
+  }
+}
