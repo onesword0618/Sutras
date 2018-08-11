ba211853a355ae12ec04c6a00d40afbeb3fc827f
onesword0618 <ao.akua.leo@gmail.com>
2018-08-11 16:27:35 +0900

    新わかりやすいJava入門編　２章サンプルコード

---
+++ new_Java/javapro/src/sample/Sample01_01.java
@@ -0,0 +1,16 @@
+// プログラムに含まれているパッケージ名を記載する。
+// 必ず１行目に記載する。
+package sample;
+
+/*
+ * 	プログラムをクラスという単位で作成します。
+ *	クラスの命名は最初が大文字の英字である。
+ */
+public class Sample01_01 {
+	// メソッド
+	// mainはプログラムの始まりを起点とする特別なメソッド
+	public static void main(String[] args) {
+		// 処理：命令内容を記載する
+		System.out.println("Hello World");
+	}
+}
---
+++ new_Java/javapro/src/sample/Sample01_01_check.java
@@ -0,0 +1,13 @@
+package sample;
+
+public class Sample01_01_check {
+
+	public static void main(String[] args) {
+		System.out.println("プログラムのかたち");
+		
+		// 練習問題１　何時ですか？　12時30分です。
+		System.out.println("何時ですか？");
+		System.out.println("12時30分です。");
+		
+	}
+}
