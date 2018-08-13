fc89c13844e6b6ee588dea813ebfa9901740ddd3
onesword0618 <ao.akua.leo@gmail.com>
2018-08-14 00:21:34 +0900

    変数宣言についてのサンプル

---
+++ new_Java/javapro/src/sample/Sample03_04.java
@@ -0,0 +1,17 @@
+package sample;
+
+public class Sample03_04 {
+
+	public static void main(String[] args) {
+		// 変数宣言
+		int number;
+		// 10を代入
+		number = 10;
+		// 20を代入
+		number = 20;
+		// 1を足す
+		number = number + 1;
+		// numberを表示する
+		System.out.println(number);
+	}
+}
