fff199b6691bac7c5fa4f89a6ddb71bcaccb4ec1
onesword0618 <ao.akua.leo@gmail.com>
2018-08-14 00:27:18 +0900

    変数宣言して加算処理を行うサンプル

---
+++ new_Java/javapro/src/sample/Sample03_05.java
@@ -0,0 +1,13 @@
+package sample;
+
+public class Sample03_05 {
+
+	public static void main(String[] args) {
+		// 宣言時の初期化
+		int a = 10;
+		// 式で初期化できる
+		int b = a+5, c;
+		c = a + b;
+		System.out.println(c);
+	}
+}
