38951e6932e1fd5f909264f30fdf1d0156206b50
onesword0618 <ao.akua.leo@gmail.com>
2018-08-14 00:00:57 +0900

    文字リテラルの書き方

---
+++ new_Java/javapro/src/sample/Sample03_03.java
@@ -0,0 +1,12 @@
+package sample;
+
+public class Sample03_03 {
+
+	public static void main(String[] args) {
+		// 文字
+		System.out.println("あ");
+		// Unicode
+		// これで問われるのはテストのときぐらいな気がする
+		System.out.println("\u3042");
+	}
+}
