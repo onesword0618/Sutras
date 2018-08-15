0bf69b47bc40496e027cf97a32a7ae119eca1eef
onesword0618 <ao.akua.leo@gmail.com>
2018-08-15 23:53:58 +0900

    junit実践入門　チュートリアル
    現場は自動生成だからなんだか新鮮な感じ

---
+++ junit.tutorial/junit.tutorial/src/main/java/junit/tutorial/Calculator.java
@@ -0,0 +1,14 @@
+package junit.tutorial;
+
+public class Calculator {
+	public int multiply(int x, int y) {
+		return x * y;
+	}
+
+	public float divide(int x, int y) {
+		if(y == 0) {
+			throw new IllegalArgumentException("divide by Zero");
+		}
+		return (float) x / (float) y;
+	}
+}
