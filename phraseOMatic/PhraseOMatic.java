40460c8e350a3cc22a826182a12f03d842d25467
onesword0618 <ao.akua.leo@gmail.com>
2019-01-09 19:36:47 +0900

    サンプルコード

---
+++ phraseOMatic/PhraseOMatic.java
@@ -0,0 +1,61 @@
+public class PhraseOMatic	{
+	public static void main (String[] args){
+	
+		String [] wordListOne = {
+			"23/7"
+			,"mlti-Tier"
+			,"30,000 foot"
+			,"B-to-B"
+			,"win-win"
+			,"front-end"
+			,"web-based"
+			,"pervasive"
+			,"smart"
+			,"six-sigma"
+			,"critical-path"
+			,"dynamic"
+		};
+		
+		String [] wordListTwo = {
+			"empowered"
+			,"sticky"
+			,"value-added"
+			,"oriented"
+			,"centric"
+			,"distributed"
+			,"pervasive"
+			,"smart"
+			,"six-sigma"
+			,"critical-path"
+			,"dynamic"
+		};
+		
+		String [] wordListThree = {
+			"23/7"
+			,"mlti-Tier"
+			,"30,000 foot"
+			,"B-to-B"
+			,"win-win"
+			,"front-end"
+			,"web-based"
+			,"pervasive"
+			,"smart"
+			,"six-sigma"
+			,"critical-path"
+			,"dynamic"
+		};
+		
+		int oneLength = wordListOne.length;
+		int twoLength = wordListTwo.length;
+		int threeLength = wordListThree.length;
+		
+		int rand1 = (int)(Math.random()*oneLength);
+		int rand2 = (int)(Math.random()*twoLength);
+		int rand3 = (int)(Math.random()*threeLength);
+		
+		String phrase =wordListOne [rand1] + " " +
+		wordListTwo [rand2] + " " + wordListThree [rand3];
+		
+		System.out.println("What we need is a" + phrase);
+	}
+}
